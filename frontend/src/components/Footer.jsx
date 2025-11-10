e6f00071513a4fe229ae42865acb26c65daf77ff
Riya Kaushik Dharsandiya <riyadharsandiya@Riyas-MacBook-Air-2.local>
2025-11-10 10:39:58 +0530

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-indigo-100 border-t border-indigo-100 text-center py-4 mt-auto">
       <p className="text-indigo-600 font-medium tracking-wide">
-        © {new Date().getFullYear()} Bhrugesh Tutorials Created by Riya Dharsandiya © 2025 All rights reserved
+        © {new Date().getFullYear()} Bhrugesh Tutorials © 2025 All rights reserved
       </p>
     </footer>
   );
