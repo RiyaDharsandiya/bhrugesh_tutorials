94c48ecd0c1d9a40eed7a4857923ae481bc5c337
Riya Kaushik Dharsandiya <riyadharsandiya@Riyas-MacBook-Air-2.local>
2026-05-13 13:22:08 +0530

    Initial commit

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -11,11 +11,25 @@ import "dotenv/config";
 
 // ---------- NODEMAILER SETUP ----------
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP SERVER READY");
+  }
 });
 
 // ---------- HELPERS ----------
